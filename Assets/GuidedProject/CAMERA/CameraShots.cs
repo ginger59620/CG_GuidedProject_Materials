592dc2b3a15ffd35430a25b2d90a995208339a56
ginger59620 <a59620@alunos.ipb.pt>
2025-12-11 11:41:43 +0000

    6. Câmaras

---
+++ Assets/GuidedProject/CAMERA/CameraShots.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraShots : MonoBehaviour
+{
+    public Camera mainCam;
+    public Camera wideCam;
+    public Camera midCam;
+    public Camera closeCam;
+    public Camera otoCam;
+
+    private void Awake()
+    {
+        mainCam.enabled = true;
+        wideCam.enabled = false;
+        midCam.enabled = false;
+        closeCam.enabled = false;
+        otoCam.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("1"))
+        {
+            Debug.Log("Olá");
+            WideCam();
+        }
+        if (Input.GetKeyUp("2"))
+        {
+            MidCam();
+        }
+        if (Input.GetKeyUp("3"))
+        {
+            CloseCam();
+        }
+        if (Input.GetKeyUp("4"))
+        {
+            OtoCam();
+        }
+        if (Input.GetKeyUp("0"))
+        {
+            MainCam();
+        }
+    }
+    public void MainCam()
+    {
+        mainCam.enabled = true;
+        wideCam.enabled = false;
+        midCam.enabled = false;
+        closeCam.enabled = false;
+        otoCam.enabled = false;
+    }
+    public void WideCam()
+    {
+        mainCam.enabled = false;
+        wideCam.enabled = true;
+        midCam.enabled = false;
+        closeCam.enabled = false;
+        otoCam.enabled = false;
+    }
+    public void CloseCam()
+    {
+        mainCam.enabled = false;
+        wideCam.enabled = false;
+        midCam.enabled = false;
+        closeCam.enabled = true;
+        otoCam.enabled = false;
+    }
+    public void MidCam()
+    {
+        mainCam.enabled = false;
+        wideCam.enabled = false;
+        midCam.enabled = true;
+        closeCam.enabled = false;
+        otoCam.enabled = false;
+    }
+    public void OtoCam()
+    {
+        mainCam.enabled = false;
+        wideCam.enabled = false;
+        midCam.enabled = false;
+        closeCam.enabled = false;
+        otoCam.enabled = true;
+    }
+}
